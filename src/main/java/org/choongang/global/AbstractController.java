3a202760e5f3f14f0bc3fd6a9893d72972902f2c
jeongsuri96@gmail.com <jeongsuri96@gmail.com>
2024-05-27 10:55:50 +0900

    템플릿 메서드 설정 업로드

---
+++ src/main/java/org/choongang/global/AbstractController.java
@@ -0,0 +1,22 @@
+package org.choongang.global;
+
+public abstract class AbstractController implements Controller {
+    /**
+     * 상단 공통 출력 부분
+     */
+    public void common(){
+
+    }
+
+    /**
+     * 입력항목
+     */
+    public void prompt(){
+
+    }
+
+    @Override
+    public final void run(){
+
+    }
+}
