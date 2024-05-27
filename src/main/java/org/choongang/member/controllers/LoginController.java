abdf265403cd48e60b455fb7ee46bc5429d55126
jeongsuri96@gmail.com <jeongsuri96@gmail.com>
2024-05-27 16:38:21 +0900

    AbstractServiceLocator.java JoinService.java LoginController.java LoginService.java MemberServiceLocator.java RequestLogin.java Service.java ServiceLocator.java

--- src/main/java/org/choongang/member/controllers/LoginController.java
+++ src/main/java/org/choongang/member/controllers/LoginController.java
@@ -2,6 +2,7 @@ package org.choongang.member.controllers;
 
 import org.choongang.global.AbstractController;
 import org.choongang.global.constants.Menu;
+import org.choongang.main.MainRouter;
 import org.choongang.template.Templates;
 
 /**
@@ -11,5 +12,20 @@ public class LoginController extends AbstractController {
     @Override
     public void show() {
         Templates.getInstance().render(Menu.LOGIN);
+    }
+
+    @Override
+    public void prompt() {
+        String userId = promptWithValidation("아이디 : ", s -> !s.isBlank());
+        String userPw = promptWithValidation("비밀번호 : ", s -> !s.isBlank());
+
+        RequestLogin form = RequestLogin.builder()
+                .userId(userId)
+                .userPw(userPw)
+                .build();
+
+        //로그인 처리...
+
+        MainRouter.getInstance().change(Menu.MAIN);
     }
 }
---
+++ src/main/java/org/choongang/member/services/MemberServiceLocator.java
@@ -0,0 +1,13 @@
+package org.choongang.member.services;
+
+import org.choongang.global.Service;
+import org.choongang.global.ServiceLocator;
+import org.choongang.global.constants.Menu;
+
+public class MemberServiceLocator implements ServiceLocator {
+    private
+    @Override
+    public Service find(Menu menu) {
+        return null;
+    }
+}
