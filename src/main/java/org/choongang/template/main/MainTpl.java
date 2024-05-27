853497fdc6a8bdbb592cd3c2f364f5995353a021
jeongsuri96@gmail.com <jeongsuri96@gmail.com>
2024-05-27 14:58:14 +0900

    setting 설계 완료

--- src/main/java/org/choongang/template/main/MainTpl.java
+++ src/main/java/org/choongang/template/main/MainTpl.java
@@ -1,6 +1,7 @@
 package org.choongang.template.main;
 
 import org.choongang.template.Template;
+import org.choongang.template.Templates;
 
 public class MainTpl implements Template {
     @Override
@@ -9,7 +10,7 @@ public class MainTpl implements Template {
         sb.append("메뉴를 선택하세요. \n")
                 .append("1. 회원가입\n")
                 .append("2. 로그인\n")
-                .append("--------------------------------------------\n");
+                .append(Templates.getInstance().line());
         return sb.toString();
     }
 }
